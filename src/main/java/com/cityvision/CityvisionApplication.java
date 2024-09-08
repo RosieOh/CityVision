f0e975182e81abcb021a76a09190e947dfbf9bbd
RosieOh <20172207@gm.hannam.ac.kr>
2024-09-08 13:23:27 +0900

    ✨BUILD : SpringBoot 3.3.3 CityVision 프로젝트 빌드

---
+++ src/main/java/com/cityvision/CityvisionApplication.java
@@ -0,0 +1,13 @@
+package com.cityvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityvisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityvisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cityvision/CityvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityvisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
